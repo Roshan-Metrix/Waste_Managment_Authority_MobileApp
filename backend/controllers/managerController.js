80e6251634dee192358e8c6d03d5a55f71021149
Roshan Patel <roshanpatel12309@gmail.com>
2025-11-15 18:30:50 +0530

    Added return store name in manager

--- backend/controllers/managerController.js
+++ backend/controllers/managerController.js
@@ -1,13 +1,16 @@
 import managerModel from "../models/managerModel.js";
+import storeModel from "../models/storeModel.js";
 
 export const getAllManagers = async (req, res) => {
   try {
     const managers = await managerModel.find().select("-password -__v");
+    const storeName = await storeModel.findOne(managers.storeId).select("-password -__v");
 
     return res.json({
       success: true,
       count: managers.length,
       managers,
+      storeName : storeName.name,
     });
 
   } catch (error) {
