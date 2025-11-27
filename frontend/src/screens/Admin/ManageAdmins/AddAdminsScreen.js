e58e01d6e70e7b47302ce5bb2dfaa8a777ec2837
Roshan Patel <roshanpatel12309@gmail.com>
2025-11-27 22:14:37 +0530

    #fixes bug

--- frontend/src/screens/Admin/ManageAdmins/AddAdminsScreen.js
+++ frontend/src/screens/Admin/ManageAdmins/AddAdminsScreen.js
@@ -33,7 +33,7 @@ export default function AddAdminsScreen({ navigation }) {
     const chars =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let pass = "";
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 10; i++) {
       pass += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return pass;
--- frontend/src/screens/Admin/ManageManagers/AddManagersScreen.js
+++ frontend/src/screens/Admin/ManageManagers/AddManagersScreen.js
@@ -32,7 +32,7 @@ export default function AddManagersScreen({ navigation }) {
     const chars =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let pass = "";
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 10; i++) {
       pass += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return pass;
